de399e0d1ad4823a4965e145a671f270840f5d7f
koehr <n@koehr.in>
2017-08-18 01:36:37 +0200

    rocket!

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,15 @@
-extern crate pencil;
-use std::collections::BTreeMap;
-use pencil::{Pencil, Request, Response, PencilResult, jsonify};
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
 
-fn app_info(_: &mut Request) -> PencilResult {
-    let mut d = BTreeMap::new();
-    d.insert("name", "nanobe");
-    d.insert("version", "0.1.0");
-    return jsonify(&d);
-}
+extern crate rocket;
 
-fn hello(_: &mut Request) -> PencilResult {
-    Ok(Response::from("Hello World!"))
-}
+mod nanobe;
+use nanobe::frontend;
+use nanobe::api;
 
 fn main() {
-    let mut app = Pencil::new("/nanobe");
-    app.get("/info", "app_info", app_info);
-    app.get("/", "hello", hello);
-    app.run("127.0.0.1:8000");
+  rocket::ignite()
+  .mount("/", routes![frontend::index])
+  .mount("/api", routes![api::index])
+  .launch();
 }
---
+++ src/nanobe/mod.rs
@@ -0,0 +1,3 @@
+pub mod api;
+pub mod frontend;
+pub mod templates;
