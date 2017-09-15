70347c4c099ffd67b14a103ecb3f39bfe6bf2468
koehr <n@koehr.in>
2017-09-15 13:25:45 +0200

    nested templating test

--- src/nanobe/template.rs
+++ src/nanobe/template.rs
@@ -1,11 +1,24 @@
 #[derive(BartDisplay)]
-#[template = "src/templates/test.html"]
-struct Template<'a> {
-    lang: &'a str,
+#[template = "src/templates/test_child.html"]
+struct Section<'a> {
     title: &'a str,
-    name: &'a str,
+    paragraphs: &'a Vec<&'a str>,
+}
+
+#[derive(BartDisplay)]
+#[template = "src/templates/test.html"]
+struct Article<'a> {
+    title: &'a str,
+    sections: &'a Vec<Section<'a>>,
 }
 
 pub fn test() {
-    print!("{}", &Template { lang: "en", title: "bart template test", name: "Bart" });
+    let paragraphs_one = vec!["section one paragraph one", "section one paragraph two"];
+    let paragraphs_two = vec!["section two paragraph one", "section two paragraph two"];
+    let sections = vec![
+      Section { title: "Section One", paragraphs: &paragraphs_one },
+      Section { title: "Section Two", paragraphs: &paragraphs_two },
+    ];
+
+    print!("{}", &Article { title: "bart template test", sections: &sections });
 }
